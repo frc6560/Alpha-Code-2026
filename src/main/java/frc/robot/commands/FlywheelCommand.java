e48e07405e107f1d62c67e506735b58a4e541ce8
kevin chou <Kevinurmomchou@icloud.com>
2026-01-20 11:41:31 -0800

    m

--- src/main/java/frc/robot/commands/FlywheelCommand.java
+++ src/main/java/frc/robot/commands/FlywheelCommand.java
@@ -1,83 +1,88 @@
 package frc.robot.commands;
 
-import frc.robot.Constants.ArmConstants;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.subsystems.superstructure.Flywheel;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
-import frc.robot.subsystems.superstructure.Arm.State;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import frc.robot.ManualControls;
 
-public class FlywheelCommand extends Command {
+public class FlywheelCommand extends Command{
     private final Flywheel flywheel;
-    private final ManualControls controls;
-    private SwerveSubsystem drivetrain;
-    
-    public FlywheelCommand(Flywheel flywheel, ManualControls controls, SwerveSubsystem drivetrain) {
+    private final SwerveSubsystem drivetrain;
+
+    private final double HOOD_DEACTUATION_TIME = 0.8; // in seconds
+    private final double TRENCH_TOLERANCE = 0.5; // in meters, larger than trench boundary
+    private final double A_ALPHA = 0.2; // acceleration filter constant
+
+    private double priorTime;
+    private Translation2d v_previous;
+    private Translation2d a;
+
+    public FlywheelCommand(Flywheel flywheel, SwerveSubsystem drivetrain) {
         this.flywheel = flywheel;
-        this.controls = controls;
         this.drivetrain = drivetrain;
         addRequirements(flywheel);
     }
 
     @Override
-public void initialize() {
-    final double t = 3.0; // time to predict ahead (get from CAD team)
-    
-    // x0 = initial pose
-    Pose2d x0 = drivetrain.getSwerveDrive().getPose();
-    
-    // v = velocity magnitude (pythag of vx and vy)
-    ChassisSpeeds robotVelocity = drivetrain.getSwerveDrive().getRobotVelocity();
-    double vx = robotVelocity.vxMetersPerSecond;
-    double vy = robotVelocity.vyMetersPerSecond;
-    double v = Math.sqrt(Math.pow(vx, 2) + Math.pow(vy, 2));
-    
-    // a = acceleration magnitude (pythag of ax and ay)
-    Translation3d accel = drivetrain.getSwerveDrive().getAccel().get();
-    double ax = accel.getX();
-    double ay = accel.getY();
-    double a = Math.sqrt(Math.pow(ax, 2) + Math.pow(ay, 2));
-    
-    // x_final = x0 + v*t + 0.5*a*t^2
-    double xFinal = x0.getX() + (vx * t) + (0.5 * ax * Math.pow(t, 2));
-    double yFinal = x0.getY() + (vy * t) + (0.5 * ay * Math.pow(t, 2));
-    
-    Translation2d futurePosition = new Translation2d(xFinal, yFinal);
-    // Current pos check: shouldn't be in a circle radius
-    // (you'll define the circle center and radius)
-    Translation2d circleCenter = new Translation2d(2,3); //change later
-    double radius = 0.0; // define this
-    
-    double distanceFromCenter = futurePosition.getDistance(circleCenter);
-    double nowdistanceFromCenter = x0.getTranslation().getDistance(circleCenter);
-    if (distanceFromCenter < radius || nowdistanceFromCenter < radius) {
-        stop();
+    public void initialize() {
+        priorTime = Timer.getFPGATimestamp();
+        v_previous = new Translation2d(
+            drivetrain.getFieldVelocity().vxMetersPerSecond,
+            drivetrain.getFieldVelocity().vyMetersPerSecond
+        );
+        a = new Translation2d();
     }
-}
+
+    /** State machine to determine robot's (future) position and how to act correspondingly */
     @Override
-    public void execute(){ 
+        public void execute() {
+        //dt 
+        double currentTime = Timer.getFPGATimestamp();
+        double dt = MathUtil.clamp(currentTime - priorTime, 0.005, 0.05);
+        priorTime = currentTime;
 
+        // numerically differentiate for acceleration. get velocity.
+        Translation2d v = new Translation2d(
+            drivetrain.getFieldVelocity().vxMetersPerSecond,
+            drivetrain.getFieldVelocity().vyMetersPerSecond
+        );
+        Translation2d a_raw = v.minus(v_previous).div(dt);
+        v_previous = v;
 
-    }
+        // filter acceleration
+        a = a.times(1 - A_ALPHA).plus(a_raw.times(A_ALPHA));
 
-    public void spinUpFlywheel(){
-        flywheel.setRPM(-1500);
-    }
+        // kiinematics!!
+        Translation2d projectedPosition = drivetrain.getPose().getTranslation()
+                                        .plus(v.times(HOOD_DEACTUATION_TIME))
+                                        .plus(a.times(HOOD_DEACTUATION_TIME * HOOD_DEACTUATION_TIME / 2.0));
 
-    public void stop(){
-        flywheel.stop();
+        if(projectedPosition.getDistance(Constants.FieldConstants.TRENCH_RED_LEFT) < TRENCH_TOLERANCE ||
+           projectedPosition.getDistance(Constants.FieldConstants.TRENCH_RED_RIGHT) < TRENCH_TOLERANCE ||
+           projectedPosition.getDistance(Constants.FieldConstants.TRENCH_BLUE_LEFT) < TRENCH_TOLERANCE ||
+           projectedPosition.getDistance(Constants.FieldConstants.TRENCH_BLUE_RIGHT) < TRENCH_TOLERANCE) {
+            flywheel.setRPM(1000);
+        } else {
+            flywheel.stop();
+        }
+
+        // logging
+        SmartDashboard.putNumber("Shooter/a_x", a.getX());
+        SmartDashboard.putNumber("Shooter/a_y", a.getY());
+        SmartDashboard.putNumber("Shooter/v_x", v.getX());
+        SmartDashboard.putNumber("Shooter/v_y", v.getY());
     }
 
     @Override
     public boolean isFinished() {
-        return false; 
+        return false;
     }
 
+    /** But this should never run lol */
     @Override
     public void end(boolean interrupted) {
         flywheel.stop();
