447dd92fa68fd396c38416d215571bdfdf60e19e
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2026-02-04 22:10:25 -0800

    better PID for aim at target

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -43,13 +43,13 @@ public final class Constants {
     public static final double kA = 0.1997;
 
     public static final double kP_translation = 2.0;
-    public static final double kP_rotation = 4.1;
+    public static final double kP_rotation = 6.0;
 
     public static final double kI_translation = 0.0;
     public static final double kI_rotation = 0.0;
 
     public static final double kD_translation = 0.2;
-    public static final double kD_rotation = 0.0; 
+    public static final double kD_rotation = 0.3; 
 
     // Pure pursuit tuning (meters, meters per second)
     public static final double kPurePursuitMinLookahead = 0.3;
